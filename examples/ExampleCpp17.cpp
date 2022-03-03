7a720f83fadc8f7218b4ef49ad2ac7b847fa84c6
Chong Yong Kin <yongkin@lp-research.com>
2022-03-03 15:02:26 +0800

    fix cpp17 example

--- examples/ExampleCpp17.cpp
+++ examples/ExampleCpp17.cpp
@@ -43,9 +43,9 @@ int main(int argc, char* argv[])
     }
 
     // check that the sensor has an IMU component
-    auto pImu = sensor.getAnyComponentOfType(g_zenSensorType_Imu);
+    auto imu = sensor.getAnyComponentOfType(g_zenSensorType_Imu);
 
-    if (!pImu)
+    if (!imu)
     {
         std::cout << "Connected sensor has no IMU" << std::endl;
         client.close();
@@ -53,14 +53,14 @@ int main(int argc, char* argv[])
     }
 
     // set and get current streaming frequency
-    auto error = imu.setInt32Property(ZenImuProperty_SamplingRate, 50);
+    auto error = imu->setInt32Property(ZenImuProperty_SamplingRate, 50);
     if (error) {
         std::cout << "Error setting streaming frequency" << std::endl;
         client.close();
         return error;
     }
 
-    auto freqPair = imu.getInt32Property(ZenImuProperty_SamplingRate);
+    auto freqPair = imu->getInt32Property(ZenImuProperty_SamplingRate);
     if (freqPair.first) {
         std::cout << "Error fetching streaming frequency" << std::endl;
         client.close();
@@ -69,7 +69,7 @@ int main(int argc, char* argv[])
     std::cout << "Streaming frequency: " << freqPair.second << std::endl;
 
     // toggle on/off of a particular data output (linAcc is not ON by default)
-    error = imu.setBoolProperty(ZenImuProperty_OutputLinearAcc, true);
+    error = imu->setBoolProperty(ZenImuProperty_OutputLinearAcc, true);
     if (error) {
         std::cout << "Error toggling ON linear acc data output" << std::endl;
         client.close();
@@ -81,7 +81,7 @@ int main(int argc, char* argv[])
     // please refer to your sensor's manual for correct retrieval of gyro data
     for (int i = 0; i < 200; i++) {
         auto event = client.waitForNextEvent();
-        if (event->component.handle == pImu->component().handle) {
+        if (event->component.handle == imu->component().handle) {
             std::cout << "> Acceleration: \t x = " << event->data.imuData.a[0]
                 << "\t y = " << event->data.imuData.a[1]
                 << "\t z = " << event->data.imuData.a[2] << std::endl;
